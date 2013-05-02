e650fa22be41dc27030023ff51bc96631dd85144
Austin Riba <austin.riba@gmail.com>
2013-05-01 22:45:21 -0700

    better ride select list

--- app/stravaese.py
+++ app/stravaese.py
@@ -10,17 +10,22 @@ def index():
 @app.route('/ride/<ride>')
 def get_ride(ride):
 	url = "".join(['http://app.strava.com/api/v1/streams/',ride])
-	response = urllib2.urlopen(url)
-	content = response.read()
-	return "%s" % content
+	return "%s" % getServerData(url)
 	
-@app.route('/rides/<userid>')
-def user_profile(userid):
-	url = "".join(['http://www.strava.com/api/v1/rides?athleteId=',userid])
-	response = urllib2.urlopen(url)
-	content = response.read()
-	return "%s" % content
+@app.route('/ride/details/<ride>')
+def get_ride_details(ride):
+	url = "".join(['http://www.strava.com/api/v2/rides/',ride])
+	return "%s" % getServerData(url)
 	
+@app.route('/rides/<userid>/<offset>')
+def user_profile(userid, offset):
+	url = "".join(['http://www.strava.com/api/v1/rides?athleteId=',userid,'&offset=',offset])
+	return "%s" % getServerData(url)
+
+def getServerData(url):
+	response = urllib2.urlopen(url)
+	return response.read()
+
 @app.route('/post/<int:some_id>')
 def adtwo(some_id):
 	return 'Times 2 %d' % (2*some_id)
